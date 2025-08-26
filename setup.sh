fd93ea70b4b289737c2f604b6cc3929a91fdf81a
ETTERI GIRI BABU <165785734+giri068@users.noreply.github.com>
2025-08-26 12:05:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,12 +42,24 @@ sudo bash -c "cat > $PORTAL_CONF" <<EOF
 </VirtualHost>
 EOF
 
-sudo a2dissite 000-default.conf
+# List all enabled sites
+ls /etc/apache2/sites-enabled/
+
+# Disable all
+sudo a2dissite '*' || true
+
 sudo a2ensite portal.conf
 sudo systemctl reload apache2
+
+sudo chown -R www-data:www-data /var/www/secure
+
+# Give proper read/write permissions
+sudo chmod 770 /var/www/secure
+sudo chmod 660 /var/www/secure/portal.db
 
 echo "[*] Setup complete!"
 echo "   🔹 Portal folder: /var/www/portal"
 echo "   🔹 Secure DB: /var/www/secure/portal.db"
 echo "   🔹 Apache site enabled: portal.conf"
 echo "[*] Captive Portal is Actively running"
+
